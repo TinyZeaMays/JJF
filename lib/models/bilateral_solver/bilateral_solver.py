506fd1f345a23cbb2b9b9efae11f786b9cbcb7ea
TinyZeaMays <1049793129@qq.com>
2023-06-13 01:47:14 +0800

    bilateral solver local

--- lib/models/bilateral_solver/bilateral_solver.py
+++ lib/models/bilateral_solver/bilateral_solver.py
@@ -70,13 +70,91 @@ def bilateral_solver(reference: np.ndarray,
     return output
 
 
+class BilateralSolverLocal(nn.Module):
+    def __init__(self,
+                 reference: np.ndarray,
+                 target: np.ndarray,
+                 sigma_space: int = 32,
+                 sigma_luma: int = 8,
+                 lam: int = 128,
+                 kernel_size: int = 21
+                 ) -> None:
+        super().__init__()
+        weight = torch.zeros((kernel_size * kernel_size - 1, 1, kernel_size, kernel_size))
+        num = 0
+        for i in range(kernel_size):
+            for j in range(kernel_size):
+                if i == j == (kernel_size - 1) // 2:
+                    continue
+                weight[num, 0, i, j] = -1
+                weight[num, 0, (kernel_size - 1) // 2, (kernel_size - 1) // 2] = 1
+                num += 1
+        self.conv = nn.Conv2d(
+            1, kernel_size * kernel_size - 1, kernel_size, padding=(kernel_size - 1) // 2, padding_mode='replicate'
+        )
+        self.conv.weight = nn.Parameter(weight, requires_grad=False)
+
+        self.image_size = (reference.shape[1], reference.shape[0])
+
+        position_x = torch.linspace(
+            0, self.image_size[0], self.image_size[0]
+        )[None, :].repeat((self.image_size[1], 1))[None, None, :, :]
+        position_y = torch.linspace(
+            0, self.image_size[1], self.image_size[1]
+        )[:, None].repeat((1, self.image_size[0]))[None, None, :, :]
+        position_x_ij = self.conv_ij(position_x)
+        position_y_ij = self.conv_ij(position_y)
+        position_ij = - (position_x_ij ** 2 + position_y_ij ** 2) / (2 * sigma_space ** 2)
+
+        reference_ij = self.conv_ij(torch.Tensor(reference)[None, None, :, :])
+        reference_ij = - reference_ij ** 2 / (2 * sigma_luma ** 2)
+
+        self.w_ij = nn.Parameter(torch.exp(position_ij + reference_ij), requires_grad=False)
+        self.target = nn.Parameter(torch.Tensor(target / 255.), requires_grad=False)
+        self.output = nn.Parameter(torch.Tensor(target / 255.), requires_grad=True)
+        self.lam = lam
+
+    def conv_ij(self, inp: Tensor) -> Tensor:
+        batch_size = inp.shape[0]
+        out = self.conv(inp)
+        return out.view((batch_size, -1))
+
+    def forward(self) -> Tensor:
+        loss = self.image_size[0] * self.image_size[1] * self.lam * torch.mean(
+            self.w_ij * self.conv_ij(self.output[None, None, :, :]) ** 2
+        ) + torch.mean((self.output - self.target) ** 2)
+        return loss
+
+
+def bilateral_solver_local(reference: np.ndarray,
+                           target: np.ndarray,
+                           sigma_space: int = 8,
+                           sigma_luma: int = 4,
+                           lam: int = 128,
+                           kernel_size: int = 21,
+                           ) -> np.ndarray:
+    solver = BilateralSolverLocal(reference, target, sigma_space, sigma_luma, lam)
+    solver.cuda()
+    optimizer = torch.optim.SGD(solver.parameters(), lr=1e-4)
+    for _ in tqdm(range(10000)):
+        optimizer.zero_grad()
+        loss = solver()
+        loss.backward()
+        optimizer.step()
+        print(loss)
+
+    output = torch.Tensor(solver.output).view((target.shape[0], target.shape[1])).detach().cpu().numpy() * 255
+
+    return output
+
+
 if __name__ == '__main__':
     import cv2
 
-    refer = cv2.imread('reference.png', 0)[512:512 + 176, 512:512 + 176]
+    refer = cv2.imread('reference.png', 0)
     # refer = cv2.resize(refer, (176, 176))
-    tgt = cv2.imread('target.png', 0)[512:512 + 176, 512:512 + 176]
+    tgt = cv2.imread('target.png', 0)
     # tgt = cv2.resize(tgt, (176, 176))
 
-    out = bilateral_solver(refer, tgt, sigma_space=8, sigma_luma=32)
+    out = bilateral_solver_local(refer, tgt)
     cv2.imwrite('result.png', out)
